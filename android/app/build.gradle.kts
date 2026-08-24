5a186433d4ccbadae0cf5c00b706269834bf4b47
incubusk104-png <incubusk104@gmail.com>
2026-08-25 00:23:42 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -86,6 +86,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
     buildFeatures {
         compose = true
         buildConfig = true
